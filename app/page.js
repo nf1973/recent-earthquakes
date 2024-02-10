f726ee6c35da4c307b4c2c86a76433d731e7166d
nf1973 <56644716+nf1973@users.noreply.github.com>
2024-02-10 22:07:42 +0100

    Use next/dynamic for maps

--- app/page.js
+++ app/page.js
@@ -2,10 +2,20 @@
 
 import { useState, useEffect } from "react";
 import DataTable from "@/components/DataTable";
-import MostRecentMap from "@/components/MostRecentMap";
-import WorldMap from "@/components/WorldMap";
 import Loading from "@/components/Loading";
 import Accreditation from "@/components/Accreditation";
+
+import dynamic from "next/dynamic";
+const DynamicWorldMap = dynamic(() => import("@/components/WorldMap"), {
+  loading: () => <p>Loading...</p>,
+});
+
+const DynamicMostRecentMap = dynamic(
+  () => import("@/components/MostRecentMap"),
+  {
+    loading: () => <p>Loading...</p>,
+  }
+);
 
 export default function Home() {
   const now = new Date();
@@ -50,10 +60,10 @@ export default function Home() {
     <main className="w-7xl max-w-7xl mx-auto flex flex-col items-center justify-start min-h-screen">
       <div className="flex flex-col lg:flex-row w-full lg:gap-4 mx-auto place-items-center">
         <div className="w-5/6 lg:w-2/3 h-96 ml-2 mr-2 lg:mr-0">
-          <WorldMap data={data} />
+          <DynamicWorldMap data={data} />
         </div>
         <div className="w-5/6 lg:w-1/3 h-96 mr-2 ml-2 lg:ml-0 mt-12 lg:mt-0 lg:mb-0">
-          <MostRecentMap data={data} />
+          <DynamicMostRecentMap data={data} />
         </div>
       </div>
       <Accreditation />
